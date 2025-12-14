8c673fa68e5d7d80f9f65d4499f8d1a581b6a982
brfialho <brfialho@student.42sp.org.br>
2025-12-14 03:52:06 -0300

    moved routine to init childs

--- philo_bonus/main.c
+++ philo_bonus/main.c
@@ -6,13 +6,14 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 11:43:06 by brfialho          #+#    #+#             */
-/*   Updated: 2025/12/14 03:43:20 by brfialho         ###   ########.fr       */
+/*   Updated: 2025/12/14 03:51:49 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 void	kill_zombies(int pid);
+void	routine(t_table *table);
 
 
 int	init_childs(t_table *table)
@@ -33,6 +34,8 @@ int	init_childs(t_table *table)
 		}
 	}
 	table->philo.id = ++i;
+	if (table->is_main == FALSE)
+		routine(table);
 	return (TRUE);
 }
 
@@ -47,17 +50,34 @@ void	kill_zombies(int n_pid)
 
 void	routine(t_table *table)
 {
-	// while (TRUE)
-	// {
-	// 	printf(PRINT, get_time(table), philo.pid, THINKING);
-	// 	usleep(1000);
-	// 	// philo_eat(philo);
-	// 	printf(PRINT, get_time(table), philo.pid, SLEEPING);
-	// 	usleep(table->input[SLEEP] * 1000);
-	// }
-	printf("%d\n", table->philo.id);
+	while (TRUE)
+	{
+		printf(PRINT, get_time(table), table->philo.id, THINKING);
+		usleep(1000);
+		// philo_eat(philo);
+		printf(PRINT, get_time(table), table->philo.id, SLEEPING);
+		usleep(table->input[SLEEP] * 1000);
+	}
 	exit(0);
 }
+
+// void	monitor(t_table *table)
+// {
+// 	int	loop;
+
+// 	loop = TRUE;
+// 	while (loop)
+// 	{
+// 		pthread_mutex_lock(&table->monitor);
+// 		if (!monitor_helper(table))
+// 		{
+// 			loop = FALSE;
+// 			table->end = TRUE;
+// 		}
+// 		pthread_mutex_unlock(&table->monitor);
+// 		usleep(1 * 100);
+// 	}
+// }
 
 int	main(int argc, char **argv)
 {
@@ -67,8 +87,6 @@ int	main(int argc, char **argv)
 	if (!table)
 		return (ERROR);
 	init_childs(table);
-	if (table->is_main == FALSE)
-	 	routine(table);
 	//monitor(table);
 	for (int i = 0; i < table->input[PHILO]; i++)
 	table->is_main ? printf("pid[%d]: %d\n", i, table->pid[i]) : 0;
